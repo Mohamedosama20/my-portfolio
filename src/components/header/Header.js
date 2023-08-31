bec1eb78f38a661ad97bb030b31850b0f204dd11
Mohamedosama20 <mohamedosama667@yahoo.com>
2023-08-31 12:11:14 +0300

    portfolio

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,24 @@ import '../../styles/header.css'
 import CTA from './CTA'
 import Me from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import Typed from 'typed.js';
+
 export const Header = () => {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Mohamed Osama'],
+      typeSpeed: 80,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <header>
       <div className='container header-container'>
         <h5>Hello, I'm</h5>
-        <h1>Mohamed Osama</h1>
+        <span ref={el}></span>
         <h5 className='text-light'>Front-End Developer</h5>
         <CTA/>
         <HeaderSocials/>
